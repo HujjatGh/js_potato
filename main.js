a32b4e80faf4385e8cd4aae0168c801f4e9f14f8
HujjatGh <hujjat.pg@gmail.com>
2023-02-07 16:11:59 +0100

    Code written

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
+let david = {name: David, currentAge: 25, maxAge: 100, amountPerWeek: 6}
+let michael = {name: Michael, currentAge: 62, maxAge: 85, amountPerWeek:10 }
+let sara = {name: Sara, currentAge: 12, maxAge: 40 , amountPerWeek: 2}
 
+function totalPotatoConsumption(name) {
+    (maxAge - currentAge) * 365/7 * amountPerWeek;
+}
+
+
+console.log(`${name} is ${currentAge} years old and will live to be ${maxAge} years old. He will take in ${totalPotatoConsumption(x)} number of potatoes till he dies.`)
